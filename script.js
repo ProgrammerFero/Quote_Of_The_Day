57cb7242142f79f11e3d1f3f1e8fd016881289ec
Programmer Fero <123647477+ProgrammerFero@users.noreply.github.com>
2023-11-30 21:54:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+/*
+Programmer Fero
+https://youtube.com/@ProgrammerFero
+*/
+
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const api_url = "https://api.quotable.io/random";
+
+async function getQuote(url) {
+    const response = await fetch(url);
+    var data = await response.json() ;
+
+    console.log(data);
+    quote.innerHTML = data.content ;
+    author.innerHTML = data.author ;
+}
+
+
+getQuote(api_url);
+
+
+/*
+Programmer Fero
+https://youtube.com/@ProgrammerFero
+*/
